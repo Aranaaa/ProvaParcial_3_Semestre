57d743469b9906961c804e367bbcac8f4dd199fc
Aranaaa <andre.arana.deziderio@gmail.com>
2021-04-22 17:02:11 -0300

    Update PessoaFisica.java

--- PessoaFisica.java
+++ PessoaFisica.java
@@ -1,11 +1,14 @@
 public class PessoaFisica extends Cliente {
     private String cpf;
     
-public void setPessoaFisica(String nome,String cpf){
+public PessoaFisica(String nome,String cpf){
+    super(nome);    
     this.cpf = cpf;
+
 }
+
 @Override
 	public String toString() {
-		return "CPF: "+this.cpf;
+		return "Nome: "+this.getNome()+"CPF: "+this.cpf;
 	}
 }
