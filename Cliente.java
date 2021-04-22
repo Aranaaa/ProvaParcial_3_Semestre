a8fe15dab6a71fa2cc49cec095267fbdaafcfd8e
Aranaaa <andre.arana.deziderio@gmail.com>
2021-04-22 17:02:06 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,8 +1,15 @@
 public abstract class Cliente {
     private String nome;
 
-public void setCliente(String nome){
+public Cliente(String nome){
     this.nome = nome;
+}
+public String getNome(){
+    return nome;
+}
+public void setNome(String nome){
+    this.nome = nome;
+    
 }
 
     @Override
