c4644859afea44eb79508243b7c764a0ef0c684d
Aranaaa <andre.arana.deziderio@gmail.com>
2021-04-22 11:18:00 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,13 @@
+public class Cliente {
+    private String nome;
+
+public void setCliente(String nome){
+    this.nome = nome;
+}
+
+    @Override
+	public String toString() {
+		return "Nome: "+this.nome;
+	}
+
+}
