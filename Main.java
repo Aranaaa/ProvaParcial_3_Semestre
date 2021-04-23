216f00b077e22a4be1755be6d0360c2011dd3c31
Aranaaa <andre.arana.deziderio@gmail.com>
2021-04-23 15:04:15 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -14,43 +14,16 @@ public class Main {
 			switch (resp) {
             case "1":
             Reservar(reservas, indice);
-				if (indice < 5) {
-					
-				} else if (indice > 4 && indice < 15 ){
-					JOptionPane.showMessageDialog(null, "Passando para a lista de espera, devido ao número de mesas cheias.", "Lista de espera",
-							JOptionPane.WARNING_MESSAGE);
-					indice += Reservar(extra, indice);
-					
-				}else {
-					JOptionPane.showMessageDialog(null, "Ta tudo lotado", "Eita!",
-							JOptionPane.WARNING_MESSAGE);
-				}
 				break;
-			case "2":
-				cpf = JOptionPane.showInputDialog("Digite o CPF que quer buscar");
-				Pesquisar(bro, extra, cpf);
-				break;
-			case "3":
-				Listar(bro);
-				Listar(bro, "27/12/2020");
-				break;
-			case "4":
-				cpf= JOptionPane.showInputDialog("Poe o cpf de quem vc quer cancelar");
-				Cancelar(bro,cpf);
-				break;
-			case "5":
-				JOptionPane.showMessageDialog(null, "Já vai tarde!");
-				break;
-
 			default:
-				JOptionPane.showMessageDialog(null, "Escolha uma opção válida.");
+			JOptionPane.showMessageDialog(null, "Escolha uma opção válida!", "Erro",JOptionPane.WARNING_MESSAGE);
 				resp = "1";
 			}
-		} while (resp.equals("1") || resp.equals("2") || resp.equals("3") || resp.equals("4")
-				|| resp.contentEquals(null));
+		} while (resp.equals("1") || resp.contentEquals(null));
 
     }
     public static void Reservar(Reserva reservas[], int indice) {
+		Cliente novo;
 		//nome
 		String nome = JOptionPane.showInputDialog("Informe seu nome");
 		//pessoafisicajuridica
@@ -59,30 +32,42 @@ public class Main {
 			JOptionPane.INFORMATION_MESSAGE, null, itens, itens[0]);
 			if (tipo == "Física") {
 				String cpf = JOptionPane.showInputDialog("Informe o CPF");
-				PessoaFisica novo = new PessoaFisica(nome, cpf);	
+				 novo = new PessoaFisica(nome, cpf);	
 				novo.setNome(nome);
 			}else if (tipo == "Jurídica") {
 				String cnpj = JOptionPane.showInputDialog("Informe o CNPJ");
-				PessoaJuridica novo = new PessoaJuridica(nome, cnpj);
+				 novo = new PessoaJuridica(nome, cnpj);
 				novo.setNome(nome);
 			} else {
-				JOptionPane.showMessageDialog(null, "Você precisa escolher uma opção!");
+				JOptionPane.showMessageDialog(null, "Escolha uma ação", "!",
+							JOptionPane.WARNING_MESSAGE);
 				return;
 			}
 		//formadepagamento
+		boolean pagamento = false;
 			Object[] items = { "A vista", "Parcelado" };
 			Object type = JOptionPane.showInputDialog(null, "Qual sua forma de pagamento?", null,
 				JOptionPane.INFORMATION_MESSAGE, null, items, items[0]);
 			if(type == "A vista"){
-			reservas[indice].setpagamentoAvista(true);
+			pagamento = true;
 			}else if (tipo == "Parcelado") {
-			reservas[indice].setpagamentoAvista(false);	
+			pagamento = false;
 
 			} else {
-				JOptionPane.showMessageDialog(null, "Você precisa escolher uma opção!");
+				JOptionPane.showMessageDialog(null, "Escolha uma ação", "!",
+							JOptionPane.WARNING_MESSAGE);
 				return;
 			}
-		
+		//reserva
+		Reserva R = new Reserva(novo, pagamento);
+		reservas[indice] = R;
+		indice++;
+			if(indice > 5){
+				JOptionPane.showMessageDialog(null, "Passando para a lista de espera, devido ao número de mesas cheias.", "Lista de espera",
+							JOptionPane.WARNING_MESSAGE);
+							
+			}else{
 		JOptionPane.showMessageDialog(null, "Reservado!", "Sucesso!", JOptionPane.INFORMATION_MESSAGE);
+	}
 	}
     }
