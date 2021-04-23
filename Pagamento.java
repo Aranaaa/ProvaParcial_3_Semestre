0e092d17fbb111dc05e8a5ecf94ac325d29a6925
Aranaaa <andre.arana.deziderio@gmail.com>
2021-04-23 12:01:19 -0300

    Create Pagamento.java

---
+++ Pagamento.java
@@ -0,0 +1,12 @@
+public class Pagamento {
+    public double calcularPagamento(boolean pagamentoAvista){
+        double auxpagamento = 0;
+        if (pagamentoAvista = true){
+            auxpagamento = 3200 * 0.9;
+        }else{
+            auxpagamento = 3200;
+    }
+    return auxpagamento;
+}
+}
+}
